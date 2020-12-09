4d2b45645ec15aea55de3f3adbc3c6419433c679
unknown <qkrdbwls613>
2020-12-09 23:25:56 +0900

    Covid19

--- chart/models.py
+++ chart/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
 # Create your models here.
+class Chart(models.Model):
+    site_name = models.CharField(max_length=100)
+    url = models.URLField('Site URL')
+
+    def __str__(self):
+        return self.title
